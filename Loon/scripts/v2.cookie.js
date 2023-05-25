d615e7419e8c575e48b3f34dc597b309480eacd4
KeithW0726 <keithwang0726@gmail.com>
2023-05-25 11:15:07 +0800

    update cookie.js

--- Loon/scripts/v2.cookie.js
+++ Loon/scripts/v2.cookie.js
@@ -1,9 +1,14 @@
 const cookieName = 'V2EX'
 const cookieKey = 'chavy_cookie_v2ex'
-const cookieVal = $request.headers['Cookie']
+const cookieVal = $request.headers['cookie']
 
 
 function get_cookie(){
+    
+    try {
+        // 请求头
+    const head = $request.headers
+    console.log(`请求头是：${head}`);
     if (cookieVal) {
         let cookie = $peristentStore.write(cookieVal, cookieKey)
         if (cookie) {
@@ -13,6 +18,10 @@ function get_cookie(){
           console.log(cookieVal)
         }
       }
+    } catch (err) {
+        console.error(`错误：${err}`);
+    }
+    
 }
 
 get_cookie({})
